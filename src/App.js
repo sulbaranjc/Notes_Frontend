e61abeecd339102de56371219cbe7b3fec09fcef
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2019-07-20 20:30:45 -0300

    componentes y rutas ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
-
+import Navigation from "./components/Navigation";
+import NotesList from "./components/NotesList";
+import CreateUser from "./components/CreateUser";
+import CreateNote from "./components/CreateNote";
 function App() {
-  return <div>Hola Mundo</div>;
+  return (
+    <Router>
+      <Navigation />
+      <Route path="/" exact component={NotesList} />
+      <Route path="/edit/:id" component={CreateNote} />
+      <Route path="/create" component={CreateNote} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
+  );
 }
 
 export default App;
